38758360e70a0c4559346369d275a6ec6ea94a36
gtindo <gtyletindo@gmail.com>
2025-01-13 18:16:07 +0100

    feat: service function to list tasks from api

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -1,3 +1,23 @@
-export async function getTasks() {}
+const BASE_URL = "http://localhost:9000/v1/tasks";
+
+/**
+ * @typedef {Object} Task
+ * @property {string} label
+ * @property {string} description
+ * @property {string} start_date
+ * @property {string} end_date
+ */
+
+/**
+ * Fetch tasks from the api
+ * @returns {Promise<Array<Task>>}
+ */
+export async function getTasks() {
+  const response = await fetch(BASE_URL);
+  if (!response.body) return [];
+  const tasks = await response.json();
+
+  return tasks;
+}
 
 export async function createTask(task) {}

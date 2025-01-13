55c342cd6da8cd082c71c22565c2747c2cf8f287
gtindo <gtyletindo@gmail.com>
2025-01-13 19:56:53 +0100

    feat: basic UI to create and list tasks

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,24 +1,63 @@
-import { attachTemplate, html } from "../shared/templates";
+import { getTasks } from "../services/tasks";
+import { createEffect, createSignal } from "../shared/signals";
+import { attachTemplate, getTargetElements, html } from "../shared/templates";
 
 class Home extends HTMLElement {
+  static targets = ["tasksList", "loader"];
+
   constructor() {
     super();
+    this.tasks = createSignal([]);
+    this.loading = createSignal(true);
+
+    this.elements = {};
   }
 
   connectedCallback() {
     attachTemplate(this, this.template());
+    this.elements = getTargetElements(this.shadowRoot, Home.targets);
+
+    // detect changes on task list and displays new list
+    createEffect(this.tasks, (tasks) => {
+      this.elements.tasksList.innerHTML = "";
+      const list = document.createElement("ul");
+
+      tasks.forEach((task) => {
+        let item = document.createElement("li");
+        item.textContent = task.label;
+        list.appendChild(item);
+      });
+
+      this.elements.tasksList.appendChild(list);
+    });
+
+    // Detect changes on loading state and display / hide loader
+    createEffect(this.loading, (loading) => {
+      this.elements.loader.style.display = loading ? "block" : "none";
+    });
+
+    this.load();
+  }
+
+  async load() {
+    this.loading.set(true);
+    const tasks = await getTasks();
+    this.tasks.set(tasks);
+    this.loading.set(false);
   }
 
   template() {
     return html`
-      <h1>Search</h1>
-      <p>List Tasks</p>
-      <p>Filter Tasks</p>
+      <section>Search bar</section>
 
-      <p>
-        Button to create a new task
+      <section>
         <x-link to="/tasks/new">Create new task</x-link>
-      </p>
+      </section>
+
+      <section>
+        <div data-target="tasksList"></div>
+        <p data-target="loader">Loading...</p>
+      </section>
     `;
   }
 }
--- src/pages/new-task.js
+++ src/pages/new-task.js
@@ -1,20 +1,51 @@
-import { attachTemplate, html } from "../shared/templates";
+import { createTask } from "../services/tasks";
+import { attachTemplate, html, getTargetElements } from "../shared/templates";
 
 class NewTasksPage extends HTMLElement {
+  static targets = ["form", "label", "description", "submitBtn", "startDate"];
+
   constructor() {
     super();
+    this.elements = {};
   }
 
   connectedCallback() {
     attachTemplate(this, this.template());
+    this.elements = getTargetElements(this.shadowRoot, NewTasksPage.targets);
+
+    this.elements.form.onsubmit = (e) => {
+      e.preventDefault();
+      this.handleSubmition();
+    };
+  }
+
+  async handleSubmition() {
+    console.log(this.elements);
+    const task = {
+      label: this.elements.label.value,
+      description: this.elements.description.value,
+      start_date: new Date(this.elements.startDate.value).toISOString(),
+    };
+
+    await createTask(task);
   }
 
   template() {
     return html`
       <h1>New Task</h1>
-      <p>Form to create a new task</p>
-
       <x-link to="/">Back to tasks</x-link>
+
+      <form data-target="form" method="post" action "/">
+        <input data-target="label" type="text" placeholder="Task label" required/>
+        <input
+          data-target="description"
+          type="text"
+          placeholder="Task description"
+          required
+        />
+        <input data-target="startDate" type="date" required />
+        <input data-target="submitBtn" type="submit" value="Create Task" />
+      </form>
     `;
   }
 }
